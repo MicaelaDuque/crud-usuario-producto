98098299c29abd6a06ee5e955b7d8e183744aba3
MicaelaDuque <micaduque@gmail.com>
2023-05-30 14:07:20 -0300

    borrado logico

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -130,7 +130,7 @@ const moviesController = {
     destroy: function (req,res) {
         let movieId = req.params.id;
         Movies
-        .destroy({where: {id: movieId}, force: true}) // force: true es para asegurar que se ejecute la acción
+        .destroy({where: {id: movieId}}) // force: true es para asegurar que se ejecute la acción
         .then(()=>{
             return res.redirect('/movies')})
         .catch(error => res.send(error)) 
